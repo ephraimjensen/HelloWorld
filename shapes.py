52e3a4459c1328dec2e0beac8f6dae9026637cc2
ephraimjensen <jen22082@byui.edu>
2024-05-08 12:01:11 -0600

    added circle area

--- shapes.py
+++ shapes.py
@@ -1,12 +1,16 @@
 # Python
-
+import math
 # This will help you to calculate a shape's area.
-
+def calculateCircleArea(radius):
+    area = math.pi * radius * radius
+    return area
 def calculateAreaRectangle(width, height):
     area=width * height
     return area
 def main():
     print("This is my shapes program")
+    circleArea = calculateCircleArea(20)
+    print(f"The area of the circle is : {circleArea}")
     rectangleArea = calculateAreaRectangle(10,20)
     print(f"The area of the rectangle is : {rectangleArea}")
 
